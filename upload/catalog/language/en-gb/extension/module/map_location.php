dc554ef60e6dc9a44eff4823c5c2b0a3295f3bfe
Nugroho Prayogo <nugrohoprayogo97@gmail.com>
2021-02-02 07:58:44 +0700

    refine language

--- upload/catalog/language/en-gb/extension/module/map_location.php
+++ upload/catalog/language/en-gb/extension/module/map_location.php
@@ -4,6 +4,8 @@ $_['text_map_location']           = 'Map Location';
 
 $_['button_edit_location']        = 'Edit Map Location';
 
+$_['button_save_location']        = 'Save Location';
+
 $_['entry_map_location']          = 'Map Location';
 
 $_['text_success_update_map']     =  'Success update map location!';        
--- upload/catalog/language/id-id/extension/module/map_location.php
+++ upload/catalog/language/id-id/extension/module/map_location.php
@@ -1,10 +1,11 @@
 <?php
-$_['text_my_location']            = "Lokasi Saya";
-$_['text_map_location']           = "Lokasi Map";
+$_['text_my_location']            = 'Lokasi Saya';
+$_['text_map_location']           = 'Lokasi Map';
 
-$_['button_edit_location']        = "Ubah Lokasi Map";
+$_['button_edit_location']        = 'Ubah Lokasi Map';
+$_['button_save_location']        = 'Simpan Lokasi';
 
-$_['entry_map_location']          = "Lokasi Map";
+$_['entry_map_location']          = 'Lokasi Map';
 
-$_['text_success_update_map']     =  'Berhasil mengubah lokasi map!'        
+$_['text_success_update_map']     =  'Berhasil mengubah lokasi map!';       
 
